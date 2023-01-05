390b20117bf80e0de3d733f2f98973596f274a6c
takealook <takealook97@naver.com>
2023-01-05 14:03:46 +0900

    commit README & java

---
+++ DataStructure.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class DataStructure {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), ":");
+        String[] getName = new String[13];
+        for (int i = 0; i < 13; i++) {
+
+        }
+    }
+}
