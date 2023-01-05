7c1a865e26781f8db66a57341a8ce5875db43e70
takealook <takealook97@naver.com>
2023-01-05 16:06:17 +0900

    DataStructure initial commit

--- DataStructure.java
+++ DataStructure.java
@@ -1,15 +1,22 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class DataStructure {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), ":");
-        String[] getName = new String[13];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] title = {"제목01", "제목02", "제목03", "제목04", "제목05", "제목06",
+                "제목07", "제목08", "제목09", "제목10", "제목11", "제목12", "제목13"};
+        String[] id = new String[13];
+        int[] time = new int[13];
         for (int i = 0; i < 13; i++) {
-
+            id[i] = "";
+            for (int j = 0; j < 8; j++) {
+                int num = (int) (Math.random() * 26 + 97);
+                id[i] += String.valueOf((char) num);
+            }
+            time[i] = (int) (Math.random() * 16 + 1);
+        }
+        for (int i = 0; i < 13; i++) {
+            System.out.println(title[i] + "(" + id[i] + "):" + time[i] + "초");
         }
     }
 }
